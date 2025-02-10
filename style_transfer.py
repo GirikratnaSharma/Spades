82f2cb7d7f6cd826bf166001a590a355978bba51
GirikratnaSharma <girikratnasharma@gmail.com>
2025-02-09 21:20:33 -0500

    extract content and style features

--- style_transfer.py
+++ style_transfer.py
@@ -4,41 +4,43 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 from PIL import Image
 
+
 def get_vgg19():
     vgg = models.vgg19(pretrained=True).features
     for param in vgg.parameters():
         param.requires_grad = False
     return vgg
 
-def load_image(image_path, max_size=400 ):
-    image = Image.open(image_path).convert("RGB")
 
+def load_image(image_path, max_size=400):
+    image = Image.open(image_path).convert("RGB")
     transform = transforms.Compose([
         transforms.Resize((max_size, max_size)),
         transforms.ToTensor()
     ])
-
     image = transform(image).unsqueeze(0)
     return image
 
-def get_features(image, model, layers=None):
-    if layers is None:
-         layers = {
-             '0': 'conv1_1',
-             '5': 'conv2_1',
-             '10': 'conv3_1',
-             '19': 'conv4_1',  
-             '28': 'conv5_1'
-         }
+
+def get_features(image, model):
+    layers = {
+        '0': 'conv1_1',
+        '5': 'conv2_1',
+        '10': 'conv3_1',
+        '19': 'content',  # Content Feature (conv4_1)
+        '28': 'conv5_1'
+    }
 
     features = {}
-
     x = image
+
     for name, layer in model._modules.items():
         x = layer(x)
         if name in layers:
             features[layers[name]] = x
+
     return features
+
 
 
 if __name__ == "__main__":
